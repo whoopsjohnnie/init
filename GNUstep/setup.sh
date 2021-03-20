f5c99208af95fc1d1eca3839b61b78f810a79e0a
whoopsjohnnie <jgrundback@gmail.com>
2021-03-20 12:28:27 -0700

    GNUstep xsession start

--- GNUstep/setup.sh
+++ GNUstep/setup.sh
@@ -17,12 +17,19 @@ sudo apt-get install -y gnustep-devel
 sudo apt-get install -y gnustep-games
 sudo apt-get install -y gnustep-icons
 
+mkdir -p ~/.themes
+mkdir -p ~/.icons
+
+mkdir -p ~/Icons
+
+cp ./home/Icons/* ~/Icons/
+
 # gtk theme
-wget -o "GTK-GNUstep.tar" "https://www.gnome-look.org/p/1079490/startdownload?file_id=1460968652&file_name=117765-GTK-GNUstep.tar&file_type=application/x-tar&file_size=307200"
+# wget -O "GTK-GNUstep.tar" "https://www.gnome-look.org/p/1079490/startdownload?file_id=1460968652&file_name=117765-GTK-GNUstep.tar&file_type=application/x-tar&file_size=307200"
 
 # gtk icons
-wget -o "areao43.tar.bz2" "https://www.gnome-look.org/p/1012496/startdownload?file_id=1460759243&file_name=101979-areao43.tar.bz2&file_type=application/x-bzip2&file_size=704758"
+# wget -O "areao43.tar.bz2" "https://www.gnome-look.org/p/1012496/startdownload?file_id=1460759243&file_name=101979-areao43.tar.bz2&file_type=application/x-bzip2&file_size=704758"
 
 # icons
-wget -o "openstepicons-1.1.tar.gz" "http://galgot.free.fr/NeXTicons/openstepicons-1.1.tar.gz"
-wget -o "areao43_folder-remote.tar.gz" "http://galgot.free.fr/NeXTicons/areao43_folder-remote.tar.gz"
+# wget -O "openstepicons-1.1.tar.gz" "http://galgot.free.fr/NeXTicons/openstepicons-1.1.tar.gz"
+# wget -O "areao43_folder-remote.tar.gz" "http://galgot.free.fr/NeXTicons/areao43_folder-remote.tar.gz"
