01891fff716f133d9df689d4f46aebdf472c7f32
whoopsjohnnie <jgrundback@gmail.com>
2021-04-06 00:26:20 -0700

    wip, installing chez scheme

--- FreeBSD/setup.sh
+++ FreeBSD/setup.sh
@@ -66,8 +66,12 @@ pkg install -y firefox
 # It would have been a version that was available for Digital UNIX on 64 bit alpha in 1997.
 # My guess is that its either mit-scheme, scm, or scheme48
 # 
+# In the pkg/mpkg ports trees, which were used during this time on the elektro Digital Unix systems, 
+# I can find the following scheme versions, umb-scheme, guile and chez
+# 
 # pkg install -y scm
 # pkg install -y scheme48
+pkg install -y chez-scheme
 pkg install -y mit-scheme
 pkg install -y guile
 
